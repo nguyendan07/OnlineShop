399721dce78759d0990cda2f862c25fc115279af
Nguyen Dan <nguyendan@hotmail.com.vn>
2017-12-06 15:09:27 +0700

    Creating shopping cart views and creating a context processor for the current cart

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
+from cart.forms import CartAddProductForm
+
 
 # Create your views here.
 def product_list(request, category_slug=None):
@@ -18,4 +20,7 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
